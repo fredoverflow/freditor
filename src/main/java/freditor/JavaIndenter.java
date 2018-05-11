c44780682d4da6fa47f3bb48cb200ab11bd4d86d
fredoverflow <fredoverflow@users.noreply.github.com>
2018-05-11 22:08:44 +0200

    pass home and end into JavaIndenter helper methods

--- src/main/java/freditor/JavaIndenter.java
+++ src/main/java/freditor/JavaIndenter.java
@@ -16,23 +16,22 @@ public abstract class JavaIndenter extends Indenter {
         int indentation = 0;
         int minimum = 0;
         for (int row = 0; row < rows; ++row) {
-            int line = text.homePositionOfRow(row);
-            corrections[row] = indentation + leadingClosers(line);
-            indentation = indentation + openersAndClosers(line);
+            int home = text.homePositionOfRow(row);
+            int end = text.endPositionOfRow(row);
+            corrections[row] = indentation + leadingClosers(home, end) - text.leadingSpaces(home);
+            indentation = indentation + openersAndClosers(home, end);
             minimum = Math.min(minimum, indentation);
         }
         for (int row = 0; row < rows; ++row) {
             corrections[row] -= minimum;
-            corrections[row] -= text.leadingSpaces(text.homePositionOfRow(row));
         }
         return corrections;
     }
 
-    private int leadingClosers(int i) {
+    private int leadingClosers(int home, int end) {
         int difference = 0;
         int space = Flexer.FIRST_SPACE;
-        final int len = text.length();
-        for (; i < len; ++i) {
+        for (int i = home; i < end; ++i) {
             int state = text.stateAt(i);
             int delta = indentationDelta(state);
             if (delta < 0) {
@@ -45,10 +44,9 @@ public abstract class JavaIndenter extends Indenter {
         return difference;
     }
 
-    private int openersAndClosers(int i) {
+    private int openersAndClosers(int home, int end) {
         int difference = 0;
-        final int end = text.endPositionOf(i);
-        for (; i < end; ++i) {
+        for (int i = home; i < end; ++i) {
             int state = text.stateAt(i);
             difference += indentationDelta(state);
         }
