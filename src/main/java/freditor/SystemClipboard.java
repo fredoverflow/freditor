1c11a12e823a329d16d3a662e91d5a40df98ea76
fredoverflow <fredoverflow@users.noreply.github.com>
2019-02-20 19:48:35 +0100

    retry and cache clipboard communication

--- src/main/java/freditor/SystemClipboard.java
+++ src/main/java/freditor/SystemClipboard.java
@@ -5,27 +5,73 @@ import java.awt.datatransfer.*;
 import java.io.IOException;
 
 public class SystemClipboard {
+    private static String internalText = null;
+    private static final int NUM_ATTEMPTS = 3;
+    private static final int SLEEP_MILLIS = 100;
+
+    private static final ClipboardOwner owner = new ClipboardOwner() {
+        @Override
+        public void lostOwnership(Clipboard clipboard, Transferable contents) {
+            // external clipboard modification
+            internalText = null;
+        }
+    };
+
     public static void set(String text) {
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        Transferable contents = new StringSelection(text);
-        clipboard.setContents(contents, null);
+        internalText = text;
+        for (int attempt = 1; attempt <= NUM_ATTEMPTS; ++attempt) {
+            try {
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                Transferable contents = new StringSelection(text);
+                clipboard.setContents(contents, owner);
+                return;
+            } catch (IllegalStateException clipboardCurrentlyUnavailable) {
+                handle(clipboardCurrentlyUnavailable, attempt);
+            }
+        }
+    }
+
+    private static void handle(IllegalStateException clipboardCurrentlyUnavailable, int attempt) {
+        if (attempt == NUM_ATTEMPTS) {
+            clipboardCurrentlyUnavailable.printStackTrace();
+        } else {
+            try {
+                Thread.sleep(SLEEP_MILLIS);
+            } catch (InterruptedException interrupted) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     public static String getUnicode() {
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        Transferable contents = clipboard.getContents(null);
-        if (contents != null) {
+        return internalText != null ? internalText : getExternalText();
+    }
+
+    private static String getExternalText() {
+        for (int attempt = 1; attempt <= NUM_ATTEMPTS; ++attempt) {
             try {
-                return (String) contents.getTransferData(DataFlavor.stringFlavor);
-            } catch (UnsupportedFlavorException | IOException ex) {
-                ex.printStackTrace();
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                Transferable contents = clipboard.getContents(null);
+                if (contents != null) {
+                    try {
+                        return internalText = (String) contents.getTransferData(DataFlavor.stringFlavor);
+                    } catch (UnsupportedFlavorException | IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                return "";
+            } catch (IllegalStateException clipboardCurrentlyUnavailable) {
+                handle(clipboardCurrentlyUnavailable, attempt);
             }
         }
         return "";
     }
 
     public static CharSequence getVisibleLatin1() {
-        String unicode = getUnicode();
+        return visibleLatin1FromUnicode(getUnicode());
+    }
+
+    private static CharSequence visibleLatin1FromUnicode(String unicode) {
         final int len = unicode.length();
         for (int i = 0; i < len; ++i) {
             if (!isVisibleLatin1(unicode.charAt(i))) {
